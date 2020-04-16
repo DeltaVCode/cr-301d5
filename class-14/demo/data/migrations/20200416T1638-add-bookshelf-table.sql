3ad476fd108f64d1ab7113aee1e0aac31301c748
Keith Dahlby <keith@newbo.co>
2020-04-16 17:18:55 -0500

    More SQL

--- class-14/demo/data/migrations/20200416T1638-add-bookshelf-table.sql
+++ class-14/demo/data/migrations/20200416T1638-add-bookshelf-table.sql
@@ -2,16 +2,31 @@
 CREATE TABLE bookshelves (id SERIAL PRIMARY KEY, name VARCHAR(255));
 
 <!-- Query 2:  Select distinct bookshelves from the books table and insert into the bookshelves table -->
-INSERT INTO bookshelves(name) SELECT DISTINCT bookshelf FROM books;
+INSERT INTO bookshelves(name)
+SELECT DISTINCT bookshelf FROM books;
 
 <!-- Query 3: Alter the books table to include a field for bookshelf id -->
 ALTER TABLE books ADD COLUMN bookshelf_id INT;
 
 <!-- Query 4: Retrieves the primary key on each bookshelf and fills in the bookshelf id field in the books table -->
-UPDATE books SET bookshelf_id=shelf.id FROM (SELECT * FROM bookshelves) AS shelf WHERE books.bookshelf = shelf.name;
+SELECT books.id, bookshelf, bookshelves.*
+FROM books
+INNER JOIN bookshelves
+  ON books.bookshelf = bookshelves.name;
+
+UPDATE books SET bookshelf_id=shelf.id
+FROM (SELECT * FROM bookshelves) AS shelf
+WHERE books.bookshelf = shelf.name;
 
 <!-- Query 5: Retrieves the primary key on each bookshelf and fills in the bookshelf id field in the books table -->
 ALTER TABLE books DROP COLUMN bookshelf;
 
 <!-- Query 6: Set the bookshelf_id as the foreign key of the books table -->
-ALTER TABLE books ADD CONSTRAINT fk_bookshelves FOREIGN KEY (bookshelf_id) REFERENCES bookshelves(id);
+ALTER TABLE books ADD CONSTRAINT fk_bookshelves
+FOREIGN KEY (bookshelf_id) REFERENCES bookshelves(id);
+
+<!-- Query 7: Get all books + their bookshelf -->
+SELECT books.id, title, name AS bookshelf
+FROM books
+INNER JOIN bookshelves
+  ON books.bookshelf_id = bookshelves.id;
